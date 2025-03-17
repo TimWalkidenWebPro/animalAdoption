f6f93b32ea3fb78424cc18094c0438b4f3fae80b
TimWalkidenWebPro <walkidentimothy@gmail.com>
2025-03-17 16:49:19 +0000

    missing file for cache

---
+++ lib/cache.ts
@@ -0,0 +1,26 @@
+import { fetchPageContent } from "@/lib/utils";
+
+type CacheEntry<T> = {
+    added: number;
+    promise: Promise<T>;
+};
+
+const cache: Record<string, CacheEntry<any>> = {};
+
+export const cachedData = <T>(prefix: string, callback: () => Promise<T>, seconds = 2): Promise<T> => {
+    const now = Date.now();
+
+    if (cache[prefix] && (now - cache[prefix].added) < seconds * 1000) {
+        console.log('Using cached data');
+        return cache[prefix].promise;
+    }
+
+    const promise = callback();
+    cache[prefix] = { added: now, promise };
+
+    return promise;
+};
+
+export const getPreData = async (prefix: string, slug: string, extraData: string[]) => {
+    return await cachedData(prefix, () => fetchPageContent(slug, extraData));
+};
